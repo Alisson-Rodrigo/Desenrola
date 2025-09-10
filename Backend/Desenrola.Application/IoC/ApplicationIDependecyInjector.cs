2546e3a956388a24b464bf2c5e2182a5f0ba1d96
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-09-10 18:31:27 -0300

    feat: implementação do login concluida com validações

---
+++ Backend/Desenrola.Application/IoC/ApplicationIDependecyInjector.cs
@@ -0,0 +1,25 @@
+﻿using Desenrola.Application.Contracts.Application;
+using Desenrola.Application.Services;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desenrola.Application.IoC
+{
+    public static class ApplicationDependencyInjector
+    {
+        public static IServiceCollection InjectApplicationDependencies(this IServiceCollection services)
+        {
+
+            services.AddScoped<ITokenService, TokenService>();
+
+
+
+            return services;
+        }
+    }
+}
--- Backend/Desenrola.Infrastructure/Abstractions/IdentityAbstractor.cs
+++ Backend/Desenrola.Infrastructure/Abstractions/IdentityAbstractor.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Desenrola.Application.Contracts.Infrastructure;
+﻿using Desenrola.Application.Contracts.Infrastructure;
 using Desenrola.Domain.Entities;
 using Desenrola.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Desenrola.Infrastructure.Abstractions;
 
@@ -10,7 +11,8 @@ namespace Desenrola.Infrastructure.Abstractions;
 /// This is a abstraction of the Identity library, creating methods that will interact with 
 /// it to create and update users
 /// </summary>
-public class IdentityAbstractor : IIdentityAbstractor {
+public class IdentityAbstractor : IIdentityAbstractor
+{
     private readonly UserManager<User> _userManager;
     private readonly SignInManager<User> _signInManager;
     private readonly RoleManager<IdentityRole> _roleManager;
@@ -19,7 +21,8 @@ public class IdentityAbstractor : IIdentityAbstractor {
         UserManager<User> userManager,
         SignInManager<User> signInManager,
         RoleManager<IdentityRole> roleManager
-    ) {
+    )
+    {
         _userManager = userManager;
         _signInManager = signInManager;
         _roleManager = roleManager;
@@ -27,16 +30,39 @@ public class IdentityAbstractor : IIdentityAbstractor {
 
     public async Task<User?> FindUserByEmailAsync(string email) => await _userManager.FindByEmailAsync(email);
 
+    public async Task<User?> FindByNameAsync(string username)
+    {
+        return await _userManager.FindByNameAsync(username);
+    }
+
+    public async Task<SignInResult> CheckPasswordSignInAsync(User user, string password)
+    {
+        return await _signInManager.CheckPasswordSignInAsync(user, password, false);
+    }
+
+    public async Task<IdentityResult> ResetPasswordAsync(User user, string decodedToken, string newPassword)
+    {
+        return await _userManager.ResetPasswordAsync(user, decodedToken, newPassword);
+    }
+
+    public async Task<IList<string>> GetRolesAsync(User user)
+    {
+        return await _userManager.GetRolesAsync(user);
+    }
+
     public async Task<User?> FindUserByIdAsync(string userId) => await _userManager.FindByIdAsync(userId);
 
     public async Task<IList<string>> GetUserRolesAsync(User user) => await _userManager.GetRolesAsync(user);
 
-    public async Task<IdentityResult> CreateUserAsync(User partnerUser, string password) {
-        if(string.IsNullOrEmpty(password)) {
+    public async Task<IdentityResult> CreateUserAsync(User partnerUser, string password)
+    {
+        if (string.IsNullOrEmpty(password))
+        {
             throw new ArgumentException($"{nameof(password)} cannot be null or empty", nameof(password));
         }
 
-        if(string.IsNullOrEmpty(partnerUser.Email)) {
+        if (string.IsNullOrEmpty(partnerUser.Email))
+        {
             throw new ArgumentException($"{nameof(User.Email)} cannot be null or empty", nameof(partnerUser));
         }
 
@@ -47,11 +73,32 @@ public class IdentityAbstractor : IIdentityAbstractor {
 
     public async Task<IdentityResult> DeleteUser(User user) => await _userManager.DeleteAsync(user);
 
-    public async Task<IdentityResult> AddToRoleAsync(User user, UserRoles role) {
-        if(await _roleManager.RoleExistsAsync(role.ToString()) is false) {
+    public async Task<IdentityResult> AddToRoleAsync(User user, UserRoles role)
+    {
+        if (await _roleManager.RoleExistsAsync(role.ToString()) is false)
+        {
             await _roleManager.CreateAsync(new IdentityRole { Name = role.ToString() });
         }
 
         return await _userManager.AddToRoleAsync(user, role.ToString());
+    }
+    public async Task<string> GeneratePasswordResetTokenAsync(User user)
+    {
+        return await _userManager.GeneratePasswordResetTokenAsync(user);
+    }
+
+    public async Task<(IEnumerable<User>, int)> GetPagedUsersAsync(int page, int pageSize)
+    {
+        int skip = (page - 1) * pageSize;
+
+        var totalUsers = await _userManager.Users.CountAsync();
+
+        var users = await _userManager.Users
+            .OrderBy(u => u.Name) // ou u.UserName, se preferir
+            .Skip(skip)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return (users, totalUsers);
     }
 }
