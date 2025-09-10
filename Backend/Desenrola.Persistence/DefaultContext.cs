d465be33f98210f5bd0ca48aaccbd02be59838b4
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-09-10 16:45:48 -0300

    feat(persistence): configurar PostgreSQL com EF Core 8 e Identity (chave string)

--- Backend/Desenrola.Persistence/DefaultContext.cs
+++ Backend/Desenrola.Persistence/DefaultContext.cs
@@ -1,16 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Desenrola.Domain.Entities;
 
 namespace Desenrola.Persistence;
 
-public class DefaultContext : IdentityDbContext<User> {
-
+public class DefaultContext : IdentityDbContext<User>
+{
     public DefaultContext() { }
 
     public DefaultContext(DbContextOptions<DefaultContext> options) : base(options) { }
 
-    protected override void OnModelCreating(ModelBuilder builder) {
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
         builder.HasPostgresExtension("uuid-ossp");
         builder.ApplyConfigurationsFromAssembly(typeof(DefaultContext).Assembly);
 
