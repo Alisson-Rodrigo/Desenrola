a174c2c4b995c8c614df19a42525c37b264054aa
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-09-10 20:24:04 -0300

    feat: implementação de controller de provider

---
+++ Backend/Desenrola.WebApi/Controllers/ProviderController.cs
@@ -0,0 +1,27 @@
+﻿using Desenrola.Application.Features.Providers.Commands.CreateProvider;
+using Desenrola.Application.Features.User.Commands.CreateUserCommand;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desenrola.WebApi.Controllers
+{
+    [Route("api/provider")]
+    [ApiController]
+    public class ProviderController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Guid), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(Guid), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateProvider(CreateProviderCommand request)
+        {
+            Guid response = await _mediator.Send(request);
+            return Created(HttpContext.Request.GetDisplayUrl(), response);
+        }
+
+    }
+}
